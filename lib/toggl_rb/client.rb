7d6d13eac3cb530b0474c3f70828938c6bb5fcad
Michael Cordell <mike@mikecordell.com>
2024-04-08 22:04:07 -0700

    Add reports connection to client

--- lib/toggl_rb/client.rb
+++ lib/toggl_rb/client.rb
@@ -5,15 +5,28 @@ require "faraday"
 module TogglRb
   # Config class stores global configuration for the TogglRb gem
   class Client
-    attr_reader :base_connection
+    attr_reader :base_connection, :reports_connection
 
     URLS = {
-      base: { url: "https://api.track.toggl.com/api/v9" }
+      base: { url: "https://api.track.toggl.com/api/v9" },
+      reports: { url: "https://api.track.toggl.com/reports/api/v3/" }
     }.freeze
 
     def initialize
       @base_connection = Faraday.new(URLS.dig(:base, :url))
+      @reports_connection = Faraday.new(URLS.dig(:reports, :url)).tap do |fc|
+        fc.set_basic_auth(config.username, config.password) if config.basic_auth?
+        fc.set_basic_auth(config.api_token, TogglRb::Config::API_TOKEN_PASSWORD) if config.api_token?
+        fc.headers["Content-Type"] = "application/json"
+        fc.headers["Accept"] = "application/json"
+      end
+    end
+
+    private
+
+    # @return [TogglRb::Config]
+    def config
+      TogglRb.config
     end
   end
 end
-
--- spec/toggl_rb/client_spec.rb
+++ spec/toggl_rb/client_spec.rb
@@ -2,12 +2,62 @@
 
 module TogglRb
   RSpec.describe Client do
-    subject(:instance) { described_class.new }
+    let(:username) { nil }
+    let(:password) { nil }
+    let(:api_token) { nil }
+    let(:basic_auth) { false }
+    let(:config_double) do
+      instance_double(TogglRb::Config,
+                      username: username,
+                      password: password,
+                      api_token: api_token,
+                      basic_auth?: basic_auth,
+                      api_token?: !!api_token)
+    end
 
-    describe "#base_connection" do
-      subject(:debug_logging) { instance.base_connection }
+    before do
+      allow(TogglRb).to receive(:config).and_return(config_double)
+    end
 
-      it { is_expected.to be_a(Faraday::Connection) }
+    describe "initialization" do
+      context "when no authentication is not configured" do
+        it "initializes base_connection and reports_connection without authentication" do
+          client = Client.new
+
+          expect(client.base_connection.headers).not_to include("Authorization")
+          expect(client.reports_connection.headers).to include("Content-Type" => "application/json",
+                                                               "Accept" => "application/json")
+          expect(client.reports_connection.headers).not_to include("Authorization")
+        end
+      end
+
+      context "when using basic auth" do
+        let(:basic_auth) { true }
+        let(:username) { "user" }
+        let(:password) { "pass" }
+
+        it "initializes reports_connection with basic auth" do
+          client = Client.new
+
+          expect(client.reports_connection.headers).to include("Content-Type" => "application/json",
+                                                               "Accept" => "application/json",
+                                                               "Authorization" => "Basic " +
+            Base64.encode64("#{username}:#{password}").chomp)
+        end
+      end
+
+      context "when using an API token" do
+        let(:api_token) { "RANDOM_STRING" }
+
+        it "initializes reports_connection with API token as basic auth" do
+          client = Client.new
+
+          expect(client.reports_connection.headers).to include("Content-Type" => "application/json",
+                                                               "Accept" => "application/json",
+                                                               "Authorization" => "Basic " +
+            Base64.encode64("#{api_token}:api_token").chomp)
+        end
+      end
     end
   end
 end
