e6a0fc297c4dc681324b6e9511f05b348e743e7b
Michael Cordell <mike@mikecordell.com>
2024-04-08 21:48:40 -0700

    Add authentication configuration options

--- lib/toggl_rb/config.rb
+++ lib/toggl_rb/config.rb
@@ -3,7 +3,21 @@
 module TogglRb
   # Config class stores global configuration for the TogglRb gem
   class Config
+    attr_accessor :username, :password, :api_token
     attr_writer :debug_logging
+
+    API_TOKEN_PASSWORD = "api_token"
+
+    # Whether username and password are set within this config in order to use basic auth. Basic auth is necessary for
+    # reports API.
+    # @return [Boolean] whether username and password exist
+    def basic_auth?
+      !!(username && password)
+    end
+
+    def api_token?
+      !!api_token
+    end
 
     # @return [Boolean] whether to log debug messages
     def debug_logging
--- spec/toggl_rb/config_spec.rb
+++ spec/toggl_rb/config_spec.rb
@@ -17,5 +17,36 @@ module TogglRb
         it { is_expected.to eq true }
       end
     end
+
+    describe "#basic_auth?" do
+      subject { instance.basic_auth? }
+
+      context "when username and password are not set" do
+        it { is_expected.to be false }
+      end
+
+      context "when both username and password are set" do
+        before do
+          instance.username = "test_username"
+          instance.password = "test_password"
+        end
+
+        it { is_expected.to be true }
+      end
+    end
+
+    describe "#api_token?" do
+      subject { instance.api_token? }
+
+      context "when api_token is not set" do
+        it { is_expected.to be false }
+      end
+
+      context "when api_token is set" do
+        before { instance.api_token = "test_api_token" }
+
+        it { is_expected.to be true }
+      end
+    end
   end
 end
