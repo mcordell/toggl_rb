4021ce75c263cd0a01a88cfcf5ffc35d5c3b408a
Michael Cordell <mike@mikecordell.com>
2024-04-11 08:25:38 -0700

    Update response object for pagination features

--- lib/toggl_rb/response.rb
+++ lib/toggl_rb/response.rb
@@ -3,6 +3,7 @@
 module TogglRb
   class Response
     extend Forwardable
+    NEXT_ROW_NUMBER_HEADER = "x-next-row-number"
 
     def initialize(farady_response)
       @farady_response = farady_response
@@ -10,6 +11,14 @@ module TogglRb
 
     def body_json
       @body_json ||= JSON.parse(@farady_response.body)
+    end
+
+    def more?
+      !!next_row_number
+    end
+
+    def next_row_number
+      @farady_response.headers[NEXT_ROW_NUMBER_HEADER]
     end
 
     def_delegators :@farady_response, :status
