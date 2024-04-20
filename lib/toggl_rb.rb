07c36efe36a478270bfe46e871d6d4d885715dc7
Michael Cordell <mike@mikecordell.com>
2024-04-20 13:48:38 -0700

    Add documentation to module

--- lib/toggl_rb.rb
+++ lib/toggl_rb.rb
@@ -13,24 +13,37 @@ require_relative "toggl_rb/reports"
 require_relative "toggl_rb/core"
 require_relative "toggl_rb/version"
 require_relative "toggl_rb/json_patch"
+require_relative "toggl_rb/request"
 
 module TogglRb
+  # A custom error class for the TogglRb module, inheriting from StandardError.
   class Error < StandardError; end
 
+  # Check if debug logging is enabled in the configuration.
+  # @return [Boolean] true if debug logging is enabled, false otherwise
   def self.debug_logging?
     config.debug_logging?
   end
 
+  # Access or initialize the configuration for the TogglRb module.
+  # This method ensures that there is only one instance of Config throughout the application lifecycle.
+  # @return [Config] the configuration instance
   def self.config
     @config ||= Config.new
   end
 
+  # Access or initialize the client used to interact with the API.
+  # This method ensures that there is only one instance of Client throughout the application lifecycle.
+  # @return [Client] the client instance
   def self.client
     @client ||= Client.new
   end
 
+  # Create a new JSONPatch object initialized with a list of operations.
+  # This method is used for generating a JSON patch based on the provided operations.
+  # @param operations [Array<Hash>] an array of operation hashes for the JSON patch
+  # @return [JSONPatch] the JSONPatch object initialized with the given operations
   def self.operation(operations = [])
     JSONPatch.new(operations)
   end
 end
-
