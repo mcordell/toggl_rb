d1cea1a85b7493d170fd8957cbc625bf225f1d81
Michael Cordell <mike@mikecordell.com>
2024-04-30 17:25:53 -0700

    Allow setting of API token on connection

--- lib/toggl_rb/client.rb
+++ lib/toggl_rb/client.rb
@@ -7,14 +7,9 @@ module TogglRb
   class Client
     attr_reader :core_connection, :reports_connection
 
-    URLS = {
-      core: { url: "https://api.track.toggl.com/api/v9" },
-      reports: { url: "https://api.track.toggl.com/reports/api/v3/" }
-    }.freeze
-
     def initialize
-      @core_connection = Connection.new(URLS.dig(:core, :url))
-      @reports_connection = Connection.new(URLS.dig(:reports, :url))
+      @core_connection = Connection.core_connection
+      @reports_connection = Connection.reports_connection
     end
   end
 end
